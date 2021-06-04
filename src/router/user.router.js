4689a5c49bb07af8d6409e6a6b50a4bb0fb8387d
wyh <kingwyh1993@163.com>
2021-06-04 12:15:43 +0800

    add: add user info

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,7 +1,60 @@
 const express = require('express');
+const crypto = require('crypto');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+// 注册
+router.post('/signin', async (req, res) => {
+  let { username, password, avatar, gender } = req.body;
+
+  let user = await User.findOne({ where: { username } });
+  if (user) {
+    res.status(400).json({ status: 400, msg: '用户名已存在' });
+    return;
+  }
+
+  let md5 = crypto.createHash('md5');
+  let cryPassword = md5.update(password).digest('hex');
+  user = await User.create({
+    username,
+    password: cryPassword,
+    avatar,
+    gender
+  });
+  res.json({ status: 200, data: { id: user.id } });
+  return;
+});
+
+// 登录
+router.post('/login', async (req, res) => {
+  let { username, password } = req.body;
+  let user = await User.findOne({ where: { username } });
+  if (user === null) {
+    res.status(400).json({ status: 400, msg: '用户名不存在' });
+    return;
+  }
+
+  let md5 = crypto.createHash('md5');
+  let cryPassword = md5.update(password).digest('hex');
+
+  if (cryPassword !== user.password) {
+    res.status(400).json({ status: 400, msg: '密码错误!' });
+    return;
+  }
+  res.json({ status: 200, data: user });
+});
+
+// 用户信息
+router.get('/info', async (req, res) => {
+  let { id = 0 } = req.body;
+  let user = await User.findOne({ where: { id } });
+  if (user === null) {
+    res.status(400).json({ status: 400, msg: '用户不存在' });
+    return;
+  }
+  res.json({ status: 200, data: user });
+});
 
 router.post('/update', async (req, res) => {
   let { id = 0, username, password, gender } = req.body;
@@ -19,10 +72,7 @@ router.post('/update', async (req, res) => {
   else {
     user = await User.findOne({ where: { id: id } });
     if (user === null) {
-      res.status(400).json({
-        status: 400,
-        msg: '未找到相关数据'
-      });
+      res.status(400).json({ status: 400, msg: '未找到相关数据' });
       return;
     }
     user = await user.update({
@@ -32,10 +82,7 @@ router.post('/update', async (req, res) => {
     });
   }
 
-  res.json({
-    status: 200,
-    data: user
-  });
+  res.json({ status: 200, data: user });
 });
 
 router.get('/list', async (req, res) => {
